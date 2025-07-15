7850849b2468e06554ec766ed1bce4a613839047
farid3dom <farid.db162@gmail.com>
2025-07-15 18:14:53 +0300

    ╨Р╨▓╤В╨╛╨╛╨▒╨╜╨╛╨▓╨╗╨╡╨╜╨╕╨╡ 15.07.2025 18:14:53,53

--- auto-push.bat
+++ auto-push.bat
@@ -1,5 +1,10 @@
 @echo off
 cd /d "C:\Users\farid\Desktop\САЙТЫ И КОДЫ\price-api-server"
+if not exist ".git" (
+  echo ❌ Проект не инициализирован как git репозиторий.
+  pause
+  exit /b
+)
 git add .
 git commit -m "Автообновление %date% %time%"
 git push origin main
