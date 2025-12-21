d5bab07ed003272b356dd49a54d122a4d4751f01
faridguseinow <farid.guseinow@gmail.com>
2025-12-21 19:25:06 +0300

    ╨Р╨▓╤В╨╛╨╛╨▒╨╜╨╛╨▓╨╗╨╡╨╜╨╕╨╡ ╨┐╤А╨░╨╣╤Б-╨╗╨╕╤Б╤В╨░

--- auto-push.bat
+++ auto-push.bat
@@ -9,5 +9,12 @@ git add . >> log.txt 2>&1
 git commit -m "Автообновление прайс-листа" >> log.txt 2>&1
 git push origin main >> log.txt 2>&1
 
+set FTP_HOST=192.168.7.108
+set FTP_USER=farid_gold
+set FTP_PASS=***** 
+
+node update-price.js >> log.txt 2>&1
+IF %ERRORLEVEL% NEQ 0 exit /b
+
 echo %date% %time% >> log.txt
 echo Обновление завершено. >> log.txt
