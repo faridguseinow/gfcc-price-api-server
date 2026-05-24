59fb2a53e7bcf446307a90c6f89d3abc523bc7e1
farid <farid.guseinow@gmail.com>
2026-05-24 12:06:50 +0300

    gf_clients

--- auto-push.bat
+++ auto-push.bat
@@ -2,19 +2,18 @@
 
 cd /d "C:\Users\User\Desktop\PROJECTS\gfcc-price-api-server-main"
 
-REM === ENV (ДО Node) ===
 set FTP_HOST=192.168.7.108
 set FTP_USER=farid_gold
 set FTP_PASS=FaridGold2025#
 
-REM === Обновление прайса ===
-node update-price.js >> log.txt 2>&1
+REM === Node update ===
+"C:\Program Files\nodejs\node.exe" update-price.js >> log.txt 2>&1
 IF %ERRORLEVEL% NEQ 0 exit /b
 
 REM === Git push ===
-git add cached_prices.json farid_gold.xml >> log.txt 2>&1
-git commit -m "price update success" >> log.txt 2>&1
-git push origin main >> log.txt 2>&1
+"C:\Program Files\Git\bin\git.exe" add cached_prices.json farid_gold.xml >> log.txt 2>&1
+"C:\Program Files\Git\bin\git.exe" commit -m "server farid_gf price updated" >> log.txt 2>&1
+"C:\Program Files\Git\bin\git.exe" push origin master >> log.txt 2>&1
 
 echo %date% %time% >> log.txt
 echo Done >> log.txt
